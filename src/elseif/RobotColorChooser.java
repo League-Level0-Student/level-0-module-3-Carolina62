c8a17d095acdfec40d5282caae8905bf6072da2d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-07-24 14:26:18 -0700

    dizzy robot, robot color chooser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -13,8 +13,15 @@ public class RobotColorChooser {
 		//3. Ask the user what color they would like the robot to draw
 		String answer = JOptionPane.showInputDialog("What color would you like the robot to draw?");
 		//5. Use an if() if/else statement to set the pen color that the user requested		
-		
-		
+        if(answer.equals("blue")) {
+        	bot.setPenColor(11, 36, 229);
+        	bot.setPenWidth(10);
+        }else 
+        	answer.equals("purple");{
+        		bot.setPenColor(145, 1, 158);
+        		bot.setPenWidth(10);
+        } 
+        	
         //6. If the user doesn’t enter anything, choose a random color
 				
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
@@ -27,6 +34,9 @@ public class RobotColorChooser {
         for (int i=0;i<4;i++) {
         bot.move(125);
         bot.turn(90);
+        
         }
+        
 	}
 }
+
