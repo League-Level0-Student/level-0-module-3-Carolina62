1e76e27bd185d4f6478068e90daea60eef7e1ac8
61653 <61653@0338S-S-R0B6ZHB.sdcs.local>
2018-12-11 14:24:13 -0800

    12/11

--- src/Checkpoint.java
+++ src/Checkpoint.java
@@ -5,14 +5,10 @@ public class Checkpoint {
 	public static void main(String[] args) {
 		
 		for (int i = 1; i < 501; i++) {
-	 if (i%5==0&& i%500==i){
-			System.out.println(i);
-			
-			 
+			if (i%2 !=0){
+				System.out.println(i);
 		}
-			
-		}
-		
-		
 	}
 }
+}
+	
